1bcede6e48986b83afd44984967892f1a1fe1c5e
Aditya Suryawanshi <suryawanshiaditya29@gmail.com>
2024-07-27 22:37:29 +0530

    part 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-let year = new Date().getFullYear()
-
 function Footer() {
-	return (
-		<footer>
-			<p>
-                Copyright ⓒ {year}
-            </p>
-        </footer>
-	);
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
